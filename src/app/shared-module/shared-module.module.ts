728bec47e98feb56b1929256e797e2db78632f2e
rishabh05 <rishabhjainwit5>
2019-02-25 12:19:50 +0530

    comming file

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LookupComponent } from '../common/lookup/lookup.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { DialogsModule } from '@progress/kendo-angular-dialog';
 
 @NgModule({  
-  imports: [ CommonModule ],
+  imports: [ CommonModule, 
+    GridModule, 
+    DialogsModule],
   declarations: [ LookupComponent ],
   exports:      [ LookupComponent ]
 })
