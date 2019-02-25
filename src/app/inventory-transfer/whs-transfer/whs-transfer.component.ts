5390f1e1731338bec5fdfc42b4510a7f05b4f3b0
Asif Ansari <asif.basecamp@gmail.com>
2019-02-25 14:55:30 +0530

    Routing related changes

--- src/app/inventory-transfer/whs-transfer/whs-transfer.component.ts
+++ src/app/inventory-transfer/whs-transfer/whs-transfer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentSidebarInfo } from 'src/app/models/sidebar/current-sidebar-info';
+import { Router } from '@angular/router';
+import { Commonservice } from 'src/app/services/commonservice.service';
 
 @Component({
   selector: 'app-whs-transfer',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./whs-transfer.component.scss']
 })
 export class WhsTransferComponent implements OnInit {
+  selectedItem: any;
 
-  constructor() { }
+  constructor(private commonService: Commonservice, private router: Router,) { }
 
   ngOnInit() {
   }
 
+  /**
+   * 
+   * @param event 
+   * @param module 
+   */
+  listClick(event, module) { 
+    this.selectedItem = module;
+    
+    this.closeRightSidebar();
+    
+    //this.router.navigate(['home/' + module]);
+    this.router.navigateByUrl('home/' + module, { skipLocationChange: true });
+
+  }
+  closeRightSidebar() {
+    let currentSidebarInfo: CurrentSidebarInfo = new CurrentSidebarInfo();
+    currentSidebarInfo.SideBarStatus = false;
+    this.commonService.setCurrentSideBar(currentSidebarInfo);
+  }
 }
