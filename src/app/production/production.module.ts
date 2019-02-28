8d50384ef7eed1ceae3310b8e79bf9c1206d6dc4
Asif Ansari <asif.basecamp@gmail.com>
2019-02-28 19:31:34 +0530

    UI changes

--- src/app/production/production.module.ts
+++ src/app/production/production.module.ts
@@ -4,12 +4,16 @@ import { CommonModule } from '@angular/common';
 import { ProductionRoutingModule } from './production-routing.module';
 import { ProductionIssueComponent } from './production-issue/production-issue.component';
 import { ProductionReceiptComponent } from './production-receipt/production-receipt.component';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { TrnaslateLazyModule } from 'src/translate-lazy.module';
 
 @NgModule({
   declarations: [ProductionIssueComponent, ProductionReceiptComponent],
   imports: [
     CommonModule,
-    ProductionRoutingModule
+    ProductionRoutingModule,
+    TrnaslateLazyModule,
+    PerfectScrollbarModule
   ]
 })
 export class ProductionModule { }
