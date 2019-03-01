8aeccdba7bf417c42deb46a7d09eca8f11aa86b6
Asif Ansari <asif.basecamp@gmail.com>
2019-03-01 12:54:29 +0530

    UI changes

--- src/app/portal-home/portal-left/portal-left.component.ts
+++ src/app/portal-home/portal-left/portal-left.component.ts
@@ -94,7 +94,6 @@ export class PortalLeftComponent implements OnInit {
     this.selectedItem = module;
     
     this.closeRightSidebar();
-    debugger;
     this.router.navigate(['home/' + module]);
   }
 
