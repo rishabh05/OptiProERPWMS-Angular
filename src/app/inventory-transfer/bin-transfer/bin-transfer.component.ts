dc9f267f529d7c77acd6fde4eaea00058f1f87db
rishabh05 <rishabhjainwit5>
2019-03-04 20:11:32 +0530

    commiting files

--- src/app/inventory-transfer/bin-transfer/bin-transfer.component.ts
+++ src/app/inventory-transfer/bin-transfer/bin-transfer.component.ts
@@ -20,22 +20,24 @@ export class BinTransferComponent implements OnInit {
   showLoader: boolean = false;
   modalRef: BsModalRef;
   showLookupLoader: boolean = true;
-  itemCode: string;
-  lotValue: string;
-  fromBin: string;
-  totalQty: string;
-  TransferQty: any;
-  itemName: string;
-  ItemTracking: string;
+  itemCode: string="";
+  lotValue: string="";
+  fromBin: string="";
+  transferQty: string="";
+  itemName: string="";
+  ItemTracking: string="";
   serviceData: any[];
   lookupfor: string;
   showItemName: boolean = false;
   showBatchNo: boolean = false;
   Remarks: string;
+  onHandQty: string;
   SysNumber: any;
   LotWhsCode: any;
-  toBin: any;
-  getDefaultBinFlag: boolean;
+  toBin: string="";
+  getDefaultBinFlag: boolean = false;
+  isItemSerialTrack: boolean;
+  editTransferQty: boolean;
 
 
   constructor(private commonservice: Commonservice, private router: Router, private inventoryTransferService: InventoryTransferService, private toastr: ToastrService, private translate: TranslateService, private modalService: BsModalService) {
@@ -57,6 +59,8 @@ export class BinTransferComponent implements OnInit {
 
     // check mobile device
     this.isMobile = UIHelper.isMobile();
+
+    localStorage.setItem("towhseId", "01");
   }
   // End UI Section
 
@@ -94,7 +98,7 @@ export class BinTransferComponent implements OnInit {
     this.inventoryTransferService.getItemCodeList().subscribe(
       data => {
         if (data != undefined && data.length > 0) {
-          console.log("ItemList - " + data);
+          console.log("ItemList - " + data.toString());
           if (data[0].ErrorMsg == "7001") {
             this.commonservice.RemoveLicenseAndSignout(this.toastr, this.router,
               this.translate.instant("CommonSessionExpireMsg"));
@@ -131,14 +135,14 @@ export class BinTransferComponent implements OnInit {
           this.showItemName = true;
           // oWhsTransEditLot.Remarks = data[0].getValue();
           this.ItemTracking = data[0].TRACKING;
-          if (data[0].TRACKING == "N") {
-            this.checkNonTrack();
-          }
+
+          this.CheckTrackingandVisiblity();
 
         } else {
           this.toastr.error('', this.translate.instant("InvalidItemCode"));
           this.showItemName = false;
           this.itemCode = "";
+          this.fromBin = "";
         }
       },
       error => {
@@ -152,7 +156,7 @@ export class BinTransferComponent implements OnInit {
     if (this.lotValue == "" || this.lotValue == undefined) {
       return;
     }
-    this.inventoryTransferService.getLotInfo("", "", this.itemCode, this.lotValue).subscribe(
+    this.inventoryTransferService.getLotInfo(this.fromBin, this.itemCode, this.lotValue).subscribe(
       data => {
         if (data != null) {
           if (data.length == 0) {
@@ -165,10 +169,9 @@ export class BinTransferComponent implements OnInit {
           }
           else {
             this.lotValue = data[0].LOTNO;
-            this.totalQty = data[0].TOTALQTY;
+            this.onHandQty = data[0].TOTALQTY;
             // oWhsTransEditLot.Qty = oCurrentController.getFormatedValue(oWhsTransEditLot.Qty);
-            // this.TransferQty = oWhsTransEditLot.Qty
-            // olblQtyOnhand.setValue(oWhsTransEditLot.Qty);
+            this.transferQty = this.onHandQty 
 
             // oWhsTransEditLot.Item = data[0].ITEMCODE;
             // oWhsTransEditLot.ITEMNAME = data[0].ITEMCODE;
@@ -187,7 +190,7 @@ export class BinTransferComponent implements OnInit {
 
 
   ShowLOTList() {
-    this.inventoryTransferService.getLotList("", "", this.itemCode, this.lotValue).subscribe(
+    this.inventoryTransferService.getLotList(localStorage.getItem("whseId"), this.fromBin, this.itemCode, this.lotValue).subscribe(
       data => {
         if (data != undefined && data.length > 0) {
           console.log("ItemList - " + data);
@@ -240,14 +243,14 @@ export class BinTransferComponent implements OnInit {
     if (this.fromBin == "" || this.fromBin == undefined) {
       return;
     }
-    this.inventoryTransferService.isFromBinExists(this.ItemTracking, "", this.itemCode, this.lotValue).subscribe(
+    this.inventoryTransferService.isFromBinExists(this.ItemTracking, this.fromBin, this.itemCode, this.lotValue).subscribe(
       data => {
         if (data != null) {
           if (data.length > 0) {
             if (this.ItemTracking == "N") {
               this.fromBin = data[0].BINNO;
-              this.TransferQty = data[0].TOTALQTY.toString();
-              this.TransferQty = data[0].TOTALQTY.toString();
+              this.onHandQty = data[0].TOTALQTY.toString();
+              this.transferQty = data[0].TOTALQTY.toString();
               // olblQtyOnhand.setValue(oCurrentController.getFormatedValue(modelBins.oData[0].TOTALQTY.toString()));
               this.SysNumber = data[0].SYSNUMBER;
               this.LotWhsCode = data[0].WHSCODE;
@@ -285,7 +288,7 @@ export class BinTransferComponent implements OnInit {
     if (this.toBin == "" || this.toBin == undefined) {
       return;
     }
-    this.inventoryTransferService.isBinExist(this.itemCode, this.lotValue).subscribe(
+    this.inventoryTransferService.isBinExist(this.toBin, "01").subscribe(
       data => {
         if (data != null) {
           if (data.length > 0) {
@@ -316,8 +319,7 @@ export class BinTransferComponent implements OnInit {
   }
 
   ShowToBins() {
-
-    this.inventoryTransferService.getToBin(this.ItemTracking, "").subscribe(
+    this.inventoryTransferService.getToBin(this.fromBin, localStorage.getItem("whseId")).subscribe(
       data => {
         if (data != null) {
           if (data.length > 0) {
@@ -347,51 +349,141 @@ export class BinTransferComponent implements OnInit {
     );
   }
 
+  IsInvTransferDetailLineExists(Item: string, LotNumber: string, Binno: string, ToBin: string, InvType: string): boolean {
+    var sumLotQuantity = 0;
+    // for (var i = 0; i < oWhsTransAddLot.Detail.length; i++) {
+    //     if (oWhsTransAddLot.Detail[i].ItemCode == Item &&
+    //            oWhsTransAddLot.Detail[i].LotNo == LotNumber &&
+    //            oWhsTransAddLot.Detail[i].BinNo == Binno &&
+    //           oWhsTransAddLot.Detail[i].ToBin == ToBin &&
+    //     oWhsTransAddLot.Detail[i].Remarks == otxtReason.getValue() &&
+    //           oWhsTransAddLot.Detail[i].InvType == InvType)
+    //         return oWhsTransAddLot.Detail[i];
+    // }
+    return false;
+  }
+
+  AddLineLots() {
+
+    var oWhsTransAddLot: any = {};
+    oWhsTransAddLot.Detail = [];
+
+    var InvDetailLine = this.IsInvTransferDetailLineExists(this.itemCode,
+    this.lotValue, this.fromBin, this.toBin, "");
+   // if (InvDetailLine == false) {
+        //debugger;
+        oWhsTransAddLot.Detail.push({
+
+            LineNum: "LineNum",
+            LotNo: this.lotValue, 
+            ItemCode: this.itemCode,
+            ItemName: this.itemName,
+            Qty: this.transferQty,
+            SysNumber: this.SysNumber,
+            BinNo: this.fromBin,
+            ToBin: this.toBin,
+            Tracking: this.ItemTracking,
+            WhsCode: localStorage.getItem("whseId"),
+            OnHandQty: this.onHandQty,
+            Remarks: this.Remarks
+            //EnableSplitContainer: oCurrentController.GetWMSDefaultValues("EnableSplitContainer"),
+            //NewConatiner: oWhsTransEditLot.Container
+        });
+   // }
+    // else {
+        if (this.ItemTracking == "S") {
+          this.toastr.error('', this.translate.instant("SerialAlreadyExist"));
+          return false;
+        }
+        else {
+            // var psOverwrite = oCurrentController.GetResourceString("WhsTransferEdit.overwrite");
+            // var psDialogConfirm = oCurrentController.GetResourceString("GoodsReceiptPOViewLots.DialogTitle");
+            // var psMsgOk = oCurrentController.GetResourceString("GoodsReceiptPOViewLots.Ok");
+            // var psMsgCancel = oCurrentController.GetResourceString("GoodsReceiptPOViewLots.Cancel");
+
+            // var dialog = new Dialog({
+            //     title: psDialogConfirm,
+            //     type: 'Message',
+            //     content: new Text({ text: psOverwrite }),
+            //     beginButton: new Button({
+            //         text: psMsgOk,
+            //         press: function () {
+            //             //InvDetailLine.Qty = (parseFloat(InvDetailLine.Qty) + parseFloat(this.GetQuantity()));
+            //             InvDetailLine.Qty = parseFloat(oCurrentController.GetQuantity());
+            //             oCurrentController.ClearModel();
+            //             oCurrentController.EnableModel();
+            //             sessionStorage.removeItem(oCurrentController.SessionProperties.InvPutAwayLot);
+            //             sessionStorage.setItem(oCurrentController.SessionProperties.InvPutAwayLot, JSON.stringify(oWhsTransAddLot));
+            //             dialog.close();
+            //         }
+            //     }),
+            //     endButton: new Button({
+            //         text: psMsgCancel,
+            //         press: function () {
+            //             dialog.close();
+            //         }
+            //     }),
+            //     afterClose: function () {
+            //         dialog.destroy();
+            //     }
+            // });
+
+            // dialog.open();
+        }
+
+       
+    // }
+
+}
+
+
   getLookupValue($event) {
     if (this.lookupfor == "ItemCodeList") {
       this.itemCode = $event[0];
-      if ($event[1].TRACKING == "N") {
-        this.checkNonTrack();
-      }
+      this.itemName  = $event[1];
+      this.ItemTracking = $event[2];
+      this.showItemName = true;
+      this.CheckTrackingandVisiblity();
     } else if (this.lookupfor == "BatchNoList") {
-
+      this.lotValue = $event[0];
+      this.fromBin  = $event[6];
+    } else if (this.lookupfor == "SBTrackFromBin") {
+      this.fromBin  = $event[3];
+    } else if (this.lookupfor == "NTrackFromBin") {
+      this.fromBin  = $event[3];
+    } else if (this.lookupfor == "toBinsList") {
+      this.toBin = $event[0];
     }
   }
 
-  checkNonTrack() {
-
-  }
-
-
   CheckTrackingandVisiblity() {
+    if (this.ItemTracking == "B") {
+      this.isItemSerialTrack = false;
+      this.showBatchNo = true;
+      this.editTransferQty = false;
+        // oTxtTransferQty.setEnabled(true);
+    }
+    else if (this.ItemTracking == "S") {
+      this.isItemSerialTrack = true;
+      this.showBatchNo = true;
+      this.editTransferQty = true;
+        // oTxtTransferQty.setEnabled(false);
+        // var qty = olblQtyOnhand.getValue();
+        // if (qty > 0) {
+        //     oWhsTransEditLot.TransferQty = oCurrentController.getFormatedValue("1");
+        // }
+        // else {
+        //     oWhsTransEditLot.TransferQty = oCurrentController.getFormatedValue("0");
+        // }
+    }
+    else if (this.ItemTracking == "N") {
+      this.isItemSerialTrack = false;
+      this.showBatchNo = false;
+        // olbllotno.setText("")
+    }
 
-    // if (this.ItemTracking == "B") {
-    //   this.showBatchNo = true;
-    //     oLotFlex.setVisible(true);
-    //     oTxtTransferQty.setEnabled(true);
-    //     olbllotqty.setText("OnHand Qty.")
-    //     olbllotno.setText("Batch No.");
-    // }
-    // else if (this.ItemTracking == "S") {
-    //   this.showBatchNo = true;
-    //     oCurrentController.getView().byId("txtLot").setVisible(true);
-    //     oTxtTransferQty.setEnabled(false);
-    //     oLotFlex.setVisible(true);
-    //     var qty = olblQtyOnhand.getValue();
-    //     if (qty > 0) {
-    //         oWhsTransEditLot.TransferQty = oCurrentController.getFormatedValue("1");
-    //     }
-    //     else {
-    //         oWhsTransEditLot.TransferQty = oCurrentController.getFormatedValue("0");
-    //     }
-    //     olbllotqty.setText("Serial Qty.")
-    //     olbllotno.setText("Serial No.");
-    // }
-    // else if (this.ItemTracking == "N") {
-    //   this.showBatchNo = false;
-    //     olbllotno.setText("")
-
-    // }
-
+    this.fromBin = "";
+    this.toBin = "";
+    this.lotValue = "";
   }
 }
