4d5dedfd9c70641a6757f4709e1737a49ad1ff0c
Asif Ansari <asif.basecamp@gmail.com>
2019-03-05 17:25:38 +0530

    New UI changes

--- src/app/inbound/inbound-details/inbound-details.component.ts
+++ src/app/inbound/inbound-details/inbound-details.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inbound-details.component.scss']
 })
 export class InboundDetailsComponent implements OnInit {
-
+  public viewLines :boolean;
   constructor() { }
 
   ngOnInit() {
