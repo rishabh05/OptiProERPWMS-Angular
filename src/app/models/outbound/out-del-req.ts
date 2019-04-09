b28a32c862f124bb36b39faac7f0c0be0b143245
Shashank Jain <sjain@optiproerp.com>
2019-04-09 19:52:03 +0530

    Outbound-Delievery

--- src/app/models/outbound/out-del-req.ts
+++ src/app/models/outbound/out-del-req.ts
@@ -1,39 +1,40 @@
 
 
-    export class SOHEADER {
-        DiServerToken: string;
-        SONumber: number;
-        CompanyDBId: string;
-        LineNo: number;
-        ShipQty: number;
-        DocNum: number;
-        OpenQty: string;
-        WhsCode: string;
-        Tracking: string;
-        ItemCode: string;
-        UOM: number;
-        Line: number;
-    }
+export class SOHEADER {
+    DiServerToken: string;
+    SONumber: number;
+    CompanyDBId: string;
+    LineNo: number;
+    ShipQty: number;
+    DocNum: number;
+    OpenQty: string;
+    WhsCode: string;
+    Tracking: string;
+    ItemCode: string;
+    UOM: number;
+    Line: number;
+}
 
-    export class SODETAIL {
-        Bin: string;
-        LotNumber: string;
-        LotQty: string;
-        SysSerial: number;
-        parentLine: number;
-        GUID: string;
-        UsernameForLic: string;
-    }
+export class SODETAIL {
+    Bin: string;
+    LotNumber: string;
+    LotQty: string;
+    SysSerial: number;
+    parentLine: number;
+    GUID: string;
+    UsernameForLic: string;
+}
 
-    export class DeliveryToken {
-        SOHEADER: SOHEADER[];
-        SODETAIL: SODETAIL[];
-        UDF: any[];
-    }
+export class DeliveryToken {
+    constructor() { }
+    SOHEADER: SOHEADER[];
+    SODETAIL: SODETAIL[];
+    UDF: any[];
+}
 
-    export class RootObject {
-        DeliveryToken: DeliveryToken;
-    }
+export class RootObject {
+    DeliveryToken: DeliveryToken;
+}
 
 
 
