10a7df58999b43ece010c9f9214b2a245aef5e51
Asif Ansari <asif.basecamp@gmail.com>
2019-04-15 15:29:50 +0530

    New UI changes

--- src/app/production/production-issue/production-issue.component.ts
+++ src/app/production/production-issue/production-issue.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-production-issue',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductionIssueComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
   }
-
+  onCancelClick() {
+    this.router.navigate(['home/dashboard']);
+  }
 }
