11eba5bd9160306c05bed5a59ea7015dd84ccaa6
Asif Ansari <asif.basecamp@gmail.com>
2019-06-14 19:38:48 +0530

    New UI changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: AppCustomPreloader,useHash: true })],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload'})],
   exports: [RouterModule],
   providers: [AppCustomPreloader]
 })
