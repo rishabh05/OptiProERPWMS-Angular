f5d11b84e995821be547f5455126101cb27a4c5b
Harinarayan Gaur <hgaur@optiproerp.com>
2019-06-24 18:22:16 +0530

    issue fixes

--- src/app/printing-label/bin-label/bin-label.component.ts
+++ src/app/printing-label/bin-label/bin-label.component.ts
@@ -174,14 +174,29 @@ export class BinLabelComponent implements OnInit {
       this.toBinInput.nativeElement.focus();
       return false;
     }
-    if (this.noOfCopies == "") {
+    if (this.noOfCopies == "" || this.noOfCopies == "0") {
       this.toastr.error('', this.translate.instant("NoOfCopiesCannotbeBlank"));
       this.noOfCopiesInput.nativeElement.focus();
       return false;
     }
+    if (!this.check_if_is_integer(this.noOfCopies)) {
+      this.toastr.error('', this.translate.instant("NoOfCopiesShouldBeNumeric"));
+      this.noOfCopiesInput.nativeElement.focus();
+      return false;
+    }
+    
     return true;
   }
 
+  check_if_is_integer(value){
+    // I can have spacespacespace1 - which is 1 and validators pases but
+    // spacespacespace doesn't - which is what i wanted.
+    // 1space2 doesn't pass - good
+    // of course, when saving data you do another parseInt.
+ 
+    return ((parseFloat(value) == parseInt(value)) && !isNaN(value));
+ 
+ }
 
   /**
    * this method will check no. of copy count value from server.
