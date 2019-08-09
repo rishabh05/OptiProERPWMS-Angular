3a6e43635a209328d694753ec213fe8b2f4e7bc2
rishabh05 <rishabhjainwit5>
2019-08-09 12:15:55 +0530

    fixed sale order list issue

--- src/app/outbound/out-order/out-order.component.ts
+++ src/app/outbound/out-order/out-order.component.ts
@@ -84,8 +84,9 @@ export class OutOrderComponent implements OnInit {
   public openOrderLookup() {
     if (this.selectedCustomer != null && this.selectedCustomer != undefined
       && this.selectedCustomer.CustomerCode != '' && this.selectedCustomer.CustomerCode != null) {
-
-      this.outboundservice.getCustomerSOList(this.selectedCustomer.CustomerCode).subscribe(
+        
+      let whseId = localStorage.getItem("whseId");
+      this.outboundservice.getCustomerSOList(this.selectedCustomer.CustomerCode, "",  whseId).subscribe(
         resp => {
           if(resp!=null){
           if (resp[0].ErrorMsg == "7001") {
