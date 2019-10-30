d7a192b6ab7c619d6a1d775856d458418da4ddcf
Harinarayan Gaur <hgaur@optiproerp.com>
2019-10-30 12:08:56 +0530

    pallet enum changes

---
+++ src/app/enums/PalletEnums.ts
@@ -0,0 +1,27 @@
+export enum PalletOperationType {
+    /// <summary>
+    /// No Type.
+    /// </summary>
+    None = 0,
+    /// <summary>
+    /// Enum to represent Split.
+    /// </summary>
+    Split = 1,
+    /// <summary>
+    /// Enum to represent Merge.
+    /// </summary>
+    Merge = 2,
+    /// <summary>
+    /// Enum to represent Depalletization.
+    /// </summary>
+    Depalletization = 3,
+    /// <summary>
+    /// Enum to represent Palletization.
+    /// </summary>
+    Palletization = 4,
+    /// <summary>
+    /// Enum to represent Transfer.
+    /// </summary>
+    Transfer = 5,
+    
+}
